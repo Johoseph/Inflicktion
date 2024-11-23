711d19b61c2988d67dd68dd386f2db3b9199ff08
Joseph Moroney <josephmoroney97@gmail.com>
2024-11-23 14:06:37 +1000

    motion for wrapper + cards

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,43 @@
-<script setup>
-import Card from './components/Card.vue'
-import cardConfig from "../card.config.json";
-</script>
-
-<template>
-  <div class="card-wrapper">
-    <Card v-for="card in cardConfig" :card="card" />
-  </div>
-</template>
-
 <style scoped>
 .card-wrapper {
   display: flex;
   justify-content: center;
   flex-shrink: 0;
+  position: relative;
+  top: 50px;
 }
 </style>
+
+<script setup>
+import { onBeforeMount, onUnmounted, ref } from "vue"
+
+import Card from './components/Card.vue'
+import cardConfig from "../card.config.json";
+
+const cardWrapperLeft = ref(0);
+
+const handleMouseMove = (e) => {
+  const horizontalCenter = document.body.clientWidth / 2;
+  const mouseCenterDelta = -(horizontalCenter - e.clientX);
+
+  // TODO: '2' here may become a prop depending on 'collapsed' view
+  // TODO: Make this nicer
+  cardWrapperLeft.value = -(mouseCenterDelta / 2);
+}
+
+onBeforeMount(() => {
+  window.addEventListener("mousemove", handleMouseMove);
+})
+
+onUnmounted(() => {
+  window.removeEventListener("mousemove", handleMouseMove);
+})
+</script>
+
+<template>
+  <div class="card-wrapper" :style="{
+    left: `${cardWrapperLeft}px`,
+  }">
+    <Card v-for="card in cardConfig" :card="card" />
+  </div>
+</template>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,19 +1,3 @@
-<template>
-  <div class="card" :style="{
-    cursor: isClickable ? `pointer` : `not-allowed`
-  }" v-html="card">
-  </div>
-</template>
-
-<script setup>
-import { defineProps } from "vue";
-
-defineProps({
-  isClickable: Boolean,
-  card: String, // stringified svg
-});
-</script>
-
 <style scoped>
 .card {
   /* 3.5 x 2.5 card ratio */
@@ -26,5 +10,30 @@ defineProps({
   /* how much a card overlaps */
   margin-right: -100px;
   box-shadow: -8px 1px 25px -1px rgba(0, 0, 0, 0.75);
+
+  position: relative;
+  bottom: 0px;
+  transition: bottom ease-out 300ms;
+}
+
+.card:hover {
+  bottom: 100px;
 }
 </style>
+
+<script setup>
+import { defineProps } from "vue";
+
+defineProps({
+  isClickable: Boolean,
+  card: String, // stringified svg
+});
+</script>
+
+
+<template>
+  <div class="card" :style="{
+    cursor: isClickable ? `pointer` : `default`
+  }" v-html="card">
+  </div>
+</template>
